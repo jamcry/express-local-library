95b4bb7d17dda318c6468550579c618b0eb131de
jamcry <jamcry@hotmail.com>
2019-08-02 23:48:35 +0300

    Complete details pages

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 var Author = require("../models/author");
+var Book = require('../models/book');
+var async = require('async');
 
 // Display list of all authors
 exports.author_list = function(req, res) {
@@ -11,8 +13,25 @@ exports.author_list = function(req, res) {
 };
 
 // Display detail page for a specific author
-exports.author_detail = function(req, res) {
-  res.send("NOT IMPLEMENTED: Author detail: " + req.params.id);
+exports.author_detail = function(req, res, next) {
+  async.parallel({
+    author: function(callback) {
+      Author.findById(req.params.id)
+        .exec(callback)
+    },
+    authors_books: function(callback) {
+      Book.find({ author: req.params.id })
+        .exec(callback)
+    },
+  }, function(err,results) {
+    if(err) return next(err);
+    if(results.author==null) {
+      var err = new Error('Author not found!');
+      err.status = 404;
+      return next(err);
+    }
+    res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books });
+  });
 };
 
 // Display Author create form on GET
