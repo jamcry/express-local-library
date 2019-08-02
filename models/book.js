40bdaf5d18cc0243fe540f821729672575a70dd3
jamcry <jamcry@hotmail.com>
2019-08-02 14:37:43 +0300

    Fix book model

--- models/book.js
+++ models/book.js
@@ -2,21 +2,22 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var BookInstanceSchema = new Schema(
+var BookSchema = new Schema(
   {
-    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, //reference to the associated book
-    imprint: {type: String, required: true},
-    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
-    due_back: {type: Date, default: Date.now}
+    title: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    summary: {type: String, required: true},
+    isbn: {type: String, required: true},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
   }
 );
 
-// Virtual for bookinstance's URL
-BookInstanceSchema
+// Virtual for book's URL
+BookSchema
 .virtual('url')
 .get(function () {
-  return '/catalog/bookinstance/' + this._id;
+  return '/catalog/book/' + this._id;
 });
 
 //Export model
-module.exports = mongoose.model('BookInstance', BookInstanceSchema);
+module.exports = mongoose.model('Book', BookSchema);
